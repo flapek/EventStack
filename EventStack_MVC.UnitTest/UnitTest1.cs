f25dd87e2c947c027eedab18ed4f854a477fd111
Marcin filapek <filap4@gmail.com>
2020-03-15 09:30:03 +0100

    Add two unit test project

---
+++ EventStack_MVC.UnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace EventStack_MVC.UnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
