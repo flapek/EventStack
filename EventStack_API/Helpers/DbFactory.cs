681783199327bd266ad10cbf7c238454b95a8248
Sherlock <tomekwator1997@gmail.com>
2020-04-01 11:58:05 +0200

    Change List to IEnumerable

--- EventStack_API/Helpers/DbFactory.cs
+++ EventStack_API/Helpers/DbFactory.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Collections.Generic;
 
 namespace EventStack_API.Helpers
@@ -5,11 +6,11 @@ namespace EventStack_API.Helpers
     abstract class DbFactory<T>
     {
         public abstract void insertOne(T insert);
-        public abstract void insertMany(List<T> insert);
+        public abstract void insertMany(IEnumerable<T> insert);
         public abstract void find(string id);
         public abstract void updateOne(T update);
-        public abstract void updateMany(List<T> update);
+        public abstract void updateMany(IEnumerable<T> update);
         public abstract void deleteOne(string id);
-        public abstract void deleteMany(List<T> delete);
+        public abstract void deleteMany(IEnumerable<T> delete);
     }
 }
