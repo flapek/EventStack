c8226644b3e91a4f4c8f49c577e8e9ccff09e5f7
Sherlock <tomekwator1997@gmail.com>
2020-04-01 12:01:28 +0200

    Change type of Id in Crud

--- EventStack_API/Helpers/DbFactory.cs
+++ EventStack_API/Helpers/DbFactory.cs
@@ -1,5 +1,8 @@
+using System.Diagnostics;
+using System;
 using System.Linq;
 using System.Collections.Generic;
+using MongoDB.Bson;
 
 namespace EventStack_API.Helpers
 {
@@ -7,10 +10,10 @@ namespace EventStack_API.Helpers
     {
         public abstract void insertOne(T insert);
         public abstract void insertMany(IEnumerable<T> insert);
-        public abstract void find(string id);
+        public abstract void find(ObjectId id);
         public abstract void updateOne(T update);
         public abstract void updateMany(IEnumerable<T> update);
-        public abstract void deleteOne(string id);
+        public abstract void deleteOne(ObjectId id);
         public abstract void deleteMany(IEnumerable<T> delete);
     }
 }
