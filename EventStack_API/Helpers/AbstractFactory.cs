9c5ce0eb102c85bb6fd813f2fd61f4ef141b5cfe
Sherlock <tomekwator1997@gmail.com>
2020-04-01 11:42:56 +0200

    Add method in AbstractFactory

--- EventStack_API/Helpers/AbstractFactory.cs
+++ EventStack_API/Helpers/AbstractFactory.cs
@@ -2,6 +2,12 @@ namespace EventStack_API.Helpers
 {
     abstract class AbstractFactory
     {
-        
+        public abstract void insertOne();
+        public abstract void insertMany();
+        public abstract void find();
+        public abstract void updateOne();
+        public abstract void updateMany();
+        public abstract void deleteOne();
+        public abstract void deleteMany();
     }
 }
