5a10e2d8ecfcc0ae15478dc76eddad4818490142
flapek <46303285+flapek@users.noreply.github.com>
2020-04-01 20:48:14 +0200

    Create constructor test

--- EventStack_API.UnitTest/DbContextTest.cs
+++ EventStack_API.UnitTest/DbContextTest.cs
@@ -1,6 +1,9 @@
 using EventStack_API.Helpers;
 using EventStack_API.Models;
 using Interfaces;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Moq;
 using NUnit.Framework;
 
 namespace EventStack_API.UnitTest
@@ -8,16 +11,35 @@ namespace EventStack_API.UnitTest
     public class DbContextTest
     {
         private DbFactory<IOrganization> dbFactory;
+        private Mock<IOptions<DbSettings>> mockOption;
+        private Mock<IMongoDatabase> mockDb;
+        private Mock<IMongoClient> mockClient;
+
         [SetUp]
         public void Setup()
         {
-            dbFactory = new DbContext();
+            mockOption = new Mock<IOptions<DbSettings>>();
+            mockDb = new Mock<IMongoDatabase>();
+            mockClient = new Mock<IMongoClient>();
         }
 
         [Test]
-        public void findMany_ReturnAllRecords_Pass()
+        public void DbContext_CreateConstructor_Success()
         {
-            Assert.Pass();
+            var settings = new DbSettings()
+            {
+                Connection = "mongodb://tes123",
+                DatabaseName = "TestDB"
+            };
+
+            mockOption.Setup(s => s.Value).Returns(settings);
+            mockClient.Setup(c => c.GetDatabase(mockOption.Object.Value.DatabaseName, null))
+                .Returns(mockDb.Object);
+
+            var contex = new DbContext(mockOption.Object);
+
+            Assert.NotNull(contex);
+
         }
     }
 }
