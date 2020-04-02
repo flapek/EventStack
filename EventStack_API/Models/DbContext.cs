26f2438706b2bc44214035fe224efa7650fa3ab7
DonOreo <44070308+DonOreo@users.noreply.github.com>
2020-04-03 00:06:40 +0200

    Remove DbFactor method's from DbContext

--- EventStack_API/Models/DbContext.cs
+++ EventStack_API/Models/DbContext.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 
 namespace EventStack_API.Models
 {
-    public class DbContext : DbFactory<Organization>
+    public class DbContext
     {
         public IMongoDatabase MongoDatabase { get; private set; }
         private MongoClient MongoClient { get; set; }
@@ -18,70 +18,5 @@ namespace EventStack_API.Models
             MongoClient = new MongoClient(configuration.Value.Connection);
             MongoDatabase = MongoClient.GetDatabase(configuration.Value.DatabaseName);
         }
-
-        public override Organization insertOne(Organization insert)
-        {
-            if (insert == null)
-                throw new ArgumentNullException();
-
-            if (insert.Id == null)
-                insert.Id = new ObjectId();
-
-            if (validModel(insert))
-            {
-                //var collection = MongoDatabase.GetCollection<Organization>("Organizaction");
-                //collection.InsertOne(insert);
-                return insert;
-            }
-
-            return null;
-        }
-
-        public override IEnumerable<Organization> insertMany(IEnumerable<Organization> insert)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override Organization find(ObjectId id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override Organization find(Organization find)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override IEnumerable<Organization> findMany(IEnumerable<Organization> find)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override Organization updateOne(Organization update)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override IEnumerable<Organization> updateMany(IEnumerable<Organization> update)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override bool deleteOne(ObjectId id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override bool deleteOne(Organization delete)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override bool deleteMany(IEnumerable<Organization> delete)
-        {
-            throw new NotImplementedException();
-        }
-
-        private bool validModel(Organization insert) => insert.Name != null && insert.Password != null && insert.Email != null;
     }
 }
