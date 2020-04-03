8879f859fda6f938571c23ae731698263768898a
flapek <46303285+flapek@users.noreply.github.com>
2020-04-03 09:35:17 +0200

    refactor class

--- EventStack_API/Helpers/DbFactory.cs
+++ EventStack_API/Helpers/DbFactory.cs
@@ -8,15 +8,15 @@ namespace EventStack_API.Helpers
 {
     public abstract class DbFactory<T>
     {
-        public abstract T insertOne(T insert);
-        public abstract IEnumerable<T> insertMany(IEnumerable<T> insert);
+        public abstract T insert(T insert);
+        public abstract IEnumerable<T> insert(IEnumerable<T> insert);
         public abstract T find(ObjectId id);
         public abstract T find(T find);
-        public abstract IEnumerable<T> findMany(IEnumerable<T> find);
-        public abstract T updateOne(T update);
-        public abstract IEnumerable<T> updateMany(IEnumerable<T> update);
-        public abstract bool deleteOne(ObjectId id);
-        public abstract bool deleteOne(T delete);
-        public abstract bool deleteMany(IEnumerable<T> delete);
+        public abstract IEnumerable<T> find(IEnumerable<T> find);
+        public abstract T update(T update);
+        public abstract IEnumerable<T> update(IEnumerable<T> update);
+        public abstract bool delete(ObjectId id);
+        public abstract bool delete(T delete);
+        public abstract bool delete(IEnumerable<T> delete);
     }
 }
