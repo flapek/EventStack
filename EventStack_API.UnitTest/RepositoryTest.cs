c669ee09c97c3b590524a42443a9a0e27bfb55cf
flapek <46303285+flapek@users.noreply.github.com>
2020-04-03 10:29:16 +0200

    test refactor

---
+++ EventStack_API.UnitTest/RepositoryTest.cs
@@ -0,0 +1,70 @@
+﻿using EventStack_API.Helpers;
+using EventStack_API.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Moq;
+using NUnit.Framework;
+using System;
+using FluentAssertions;
+using Models;
+
+namespace EventStack_API.UnitTest
+{
+    public class RepositoryTest
+    {
+        private DbFactory<Organization> dbFactory;
+        private Mock<DbContext> dbContextMock;
+        private Mock<IOptions<DbSettings>> mockOption;
+
+        [SetUp]
+        public void Setup()
+        {
+            var settings = new DbSettings()
+            {
+                _connectionString = "mongodb://tes123",
+                _databaseName = "TestDB"
+            };
+
+            mockOption = new Mock<IOptions<DbSettings>>();
+            mockOption.Setup(s => s.Value).Returns(settings);
+
+            dbFactory = new OrganizationRepository(dbContextMock.Object);
+        }
+
+        #region insert Test
+
+        [Test]
+        public void insert_WhenInputIsNull_ThenArgumentNullExceptionIsThrown()
+        {
+            Action action = () => dbFactory.insert(new Organization());
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [TestCase("Jan", "@j3st1234", "jan.test@test.com")]
+        public void insert_WhenInputIdIsNotSet_ThenGenerateId(string name, string password, string email) =>
+            dbFactory.insert(new Organization() { Name = name, Password = password, Email = email }).Id.Should().NotBeNull();
+
+        [Test]
+        [Combinatorial]
+        public void insert_WhenNameOrPasswordOrEmailIsNull_ThenReturnNull(
+            [Values(null, "Jan")] string name,
+            [Values(null, "@j3st1234")] string password,
+            [Values(null, "jan.test@test.com")] string email)
+        {
+            if (name != null && password != null && email != null)
+                return;
+
+            var result = dbFactory.insert(new Organization() { Name = name, Password = password, Email = email });
+            result.Should().BeNull();
+        }
+
+        [TestCase("Jan", "@j3st1234", "jan.test@test.com")]
+        public void insert_WhenNameOrPasswordOrEmailIsNotNull_ThenReturnOrganizaction(string name, string password, string email)
+        {
+            var expected = new Organization() { Name = name, Password = password, Email = email };
+            dbFactory.insert(expected).Should().BeSameAs(expected);
+        }
+
+        #endregion
+    }
+}
