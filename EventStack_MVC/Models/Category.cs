25c23636c572a00f4f8c8d1d9bb1a6e3edfaa25b
flapek <46303285+flapek@users.noreply.github.com>
2020-04-04 21:20:03 +0200

    copy model to MVC project

--- EventStack_MVC/Models/Category.cs
+++ EventStack_MVC/Models/Category.cs
@@ -1,15 +1,23 @@
+using EventStack_API.Interfaces;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace Models
 {
-    public class Category 
+    public class Category : IDbModel
     {
         [BsonId]
         [BsonElement("Id")]
         [BsonRepresentation(BsonType.ObjectId)]
+        [BsonRequired]
+        [Required(ErrorMessage = "Id must be defined!")]
         public ObjectId Id { get; set; }
+
         [BsonElement("Name")]
+        [BsonRequired]
+        [Required(ErrorMessage = "Name must be defined!")]
+        [StringLength(50, ErrorMessage = "The maximum number of character is 50!")]
         public string Name { get; set; }
     } 
 }
