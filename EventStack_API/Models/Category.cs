4d39fb0b60b405f0cce19814d8f00b45461c9f09
flapek <46303285+flapek@users.noreply.github.com>
2020-04-04 20:51:23 +0200

    Add validation decorator to category model

--- EventStack_API/Models/Category.cs
+++ EventStack_API/Models/Category.cs
@@ -1,6 +1,7 @@
 using EventStack_API.Interfaces;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace Models
 {
@@ -9,8 +10,13 @@ namespace Models
         [BsonId]
         [BsonElement("Id")]
         [BsonRepresentation(BsonType.ObjectId)]
+        [BsonRequired()]
+        [Required(ErrorMessage = "Id must be defined!")]
         public ObjectId Id { get; set; }
+
         [BsonElement("Name")]
+        [BsonRequired()]
+        [Required(ErrorMessage = "Name must be defined!")]
         public string Name { get; set; }
     } 
 }
