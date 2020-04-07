ff8c26f92d0f93e78699fd4c066953848f1099e6
k0l099 <k0l012@interia.pl>
2020-04-08 01:49:11 +0200

    Create class PasswordHasher

---
+++ EventStack_API/Helpers/PasswordHasher.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Security.Cryptography;
+
+public class PasswordHasher
+{
+    public static string ComputeHash(string password)
+    {
+        var salt = "3v3nt.St@ck";
+        var bytesPassword = Convert.FromBase64String(password + salt);
+
+        var hashAlgorithm = new SHA512Managed();
+        var hashedPassword = hashAlgorithm.ComputeHash(bytesPassword);
+
+        return Convert.ToBase64String(hashedPassword);
+    }
+}
