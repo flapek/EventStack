ea4637f660f54e07437c46ad02a91d9bebd2a3a4
Sherlock <tomekwator1997@gmail.com>
2020-04-08 02:56:43 +0200

    Change variable salt to const

--- EventStack_API/Helpers/PasswordHasher.cs
+++ EventStack_API/Helpers/PasswordHasher.cs
@@ -3,9 +3,10 @@ using System.Security.Cryptography;
 
 public class PasswordHasher
 {
+    private const string salt = "3v3nt.St@ck";
+
     public static string ComputeHash(string password)
     {
-        var salt = "3v3nt.St@ck";
         var bytesPassword = Convert.FromBase64String(password + salt);
 
         var hashAlgorithm = new SHA512Managed();
