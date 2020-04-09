cd7a07a908105150945da3fbdd603f3e882ac394
DonOreo <44070308+DonOreo@users.noreply.github.com>
2020-04-10 01:26:06 +0200

    Clean code - Remove unused namespace

--- EventStack_API/Models/Organization.cs
+++ EventStack_API/Models/Organization.cs
@@ -1,6 +1,5 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
-using Geolocation; // https://github.com/scottschluer/geolocation
 using EventStack_API.Interfaces;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
