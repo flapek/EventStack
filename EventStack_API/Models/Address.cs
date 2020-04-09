2a63cdb0cae94fccd1cd948b6c6d4bb03ce7052d
DonOreo <44070308+DonOreo@users.noreply.github.com>
2020-04-10 01:35:53 +0200

    Create properties for Address Class

--- EventStack_API/Models/Address.cs
+++ EventStack_API/Models/Address.cs
@@ -1,7 +1,15 @@
+using EventStack_API.Interfaces;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
 namespace EventStack_API.Models
 {
     public class Address
     {
-        
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
     }
 }
