6956b1dd2a6d6d3adfdec3e175438cc8394438ea
DonOreo <44070308+DonOreo@users.noreply.github.com>
2020-04-10 01:52:16 +0200

    Add bson attributes

--- EventStack_API/Models/Address.cs
+++ EventStack_API/Models/Address.cs
@@ -7,9 +7,23 @@ namespace EventStack_API.Models
 {
     public class Address
     {
+        [BsonId]
+        [BsonElement("Id")]
+        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonRequired]
+        [Required(ErrorMessage = "Id must be defined!")]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("Country")]
         public string Country { get; set; }
+
+        [BsonElement("City")]
         public string City { get; set; }
+
+        [BsonElement("Street")]
         public string Street { get; set; }
+
+        [BsonElement("ZipCode")]
         public string ZipCode { get; set; }
     }
 }
