c57147d77102fb72117eb56f6d7027e6851133d5
flapek <46303285+flapek@users.noreply.github.com>
2020-04-11 17:52:16 +0200

    Add interface for DbSettings

--- EventStack_API/Models/DbSettings.cs
+++ EventStack_API/Models/DbSettings.cs
@@ -1,6 +1,8 @@
-﻿namespace EventStack_API.Models
+﻿using EventStack_API.Interfaces;
+
+namespace EventStack_API.Models
 {
-    public class DbSettings
+    public class DbSettings : IDbSettings
     {
         public string ConnectionString { get; set; }
         public string DatabaseName { get; set; }
