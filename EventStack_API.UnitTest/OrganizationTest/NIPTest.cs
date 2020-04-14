05cd87a069524d7052e1b6c0be0c9ef71a6306d5
DonOreo <44070308+DonOreo@users.noreply.github.com>
2020-04-14 03:48:43 +0200

    Add unitTest IsRegexAcceptNIP

--- EventStack_API.UnitTest/OrganizationTest/NIPTest.cs
+++ EventStack_API.UnitTest/OrganizationTest/NIPTest.cs
@@ -16,5 +16,20 @@ namespace EventStack_API.UnitTest.OrganizationTest
         {
             organization = new Organization();
         }
+
+        [TestCase("1234567890")]
+        public void Organization_IsRegexAcceptNIP_False(string NIP)
+        {
+            organization.NIP = NIP;
+            Assert.IsFalse(ValidateModel(organization).Any(a => a.MemberNames.Contains("NIP") && a.ErrorMessage.Contains("NIP must contain")));
+        }
+
+        private IList<ValidationResult> ValidateModel(object model)
+        {
+            var validationResults = new List<ValidationResult>();
+            var ctx = new ValidationContext(model, null, null);
+            Validator.TryValidateObject(model, ctx, validationResults, true);
+            return validationResults;
+        }
     }
 }
