36d7d6e8f06738191b44d0a29d3875460e728754
DonOreo <44070308+DonOreo@users.noreply.github.com>
2020-04-14 02:46:37 +0200

    Add unitTest isIdRequired and CanBeNotNull

--- EventStack_API.UnitTest/OrganizationTest/IdTest.cs
+++ EventStack_API.UnitTest/OrganizationTest/IdTest.cs
@@ -16,5 +16,27 @@ namespace EventStack_API.UnitTest.OrganizationTest
         {
             organization = new Organization();
         }
+
+        [Test]
+        public void Organization_IsIdRequired_True()
+        {
+            organization.Id = null;
+            Assert.IsTrue(ValidateModel(organization).Any(a => a.MemberNames.Contains("Id") && a.ErrorMessage.Contains("Id must be defined!")));
+        }
+
+        [Test]
+        public void Organization_IsIdCanBeNotNull_False()
+        {
+            organization.Id = "not null";
+            Assert.IsFalse(ValidateModel(organization).Any(a => a.MemberNames.Contains("Id") && a.ErrorMessage.Contains("Id must be defined!")));
+        }
+
+        private IList<ValidationResult> ValidateModel(object model)
+        {
+            var validationResults = new List<ValidationResult>();
+            var ctx = new ValidationContext(model, null, null);
+            Validator.TryValidateObject(model, ctx, validationResults, true);
+            return validationResults;
+        }
     }
 }
