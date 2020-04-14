8aaeb2a58ad0d1d93b77c0ffbd4cd547d8c33731
DonOreo <44070308+DonOreo@users.noreply.github.com>
2020-04-14 03:32:38 +0200

    Create class REGONTest and SetUp method

---
+++ EventStack_API.UnitTest/OrganizationTest/REGONTest.cs
@@ -0,0 +1,20 @@
+using System.Linq;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using EventStack_API.Models;
+using NUnit.Framework;
+
+namespace EventStack_API.UnitTest.OrganizationTest
+{
+    public class REGONTest
+    {
+        private Organization organization;
+
+        [SetUp]
+        public void SetUp()
+        {
+            organization = new Organization();
+        }
+    }
+}
