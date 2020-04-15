6969bd6879f1a1574c404a484a6f0ece5c9db2cf
flapek <46303285+flapek@users.noreply.github.com>
2020-04-15 16:34:56 +0200

    implement all method in controller

--- EventStack_API/Controllers/CategoryController.cs
+++ EventStack_API/Controllers/CategoryController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using EventStack_API.Interfaces;
 using EventStack_API.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EventStack_API.Controllers
@@ -20,6 +16,59 @@ namespace EventStack_API.Controllers
         {
             this.repository = repository;
         }
+
+        // GET: api/Category/id
+        [HttpGet("{id}", Name = "Get")]
+        public Category Get(string id)
+            => repository.Find(id);
+
+        // GET: api/Category
+        [HttpGet]
+        public Category Get(Category organization)
+            => repository.Find(organization);
+
+        // GET: api/Category
+        [HttpGet]
+        public IEnumerable<Category> Get(IEnumerable<Category> organization)
+            => repository.Find(organization);
+
+        // POST: api/Category
+        [HttpPost]
+        public bool Post(Category organizaction)
+            => ModelState.IsValid ? repository.Insert(organizaction) : false;
+
+        // POST: api/Category
+        [HttpPost]
+        public bool Post(IEnumerable<Category> organizactions)
+            => ModelState.IsValid ? repository.Insert(organizactions) : false;
+
+        // PUT: api/Category
+        [HttpPut("{id}")]
+        public bool Put(Category organization)
+            => ModelState.IsValid ? repository.Update(organization) : false;
+
+        // PUT: api/Category
+        [HttpPut("{id}")]
+        public bool Put(IEnumerable<Category> organizations)
+            => ModelState.IsValid ? repository.Update(organizations) : false;
+
+
+        // DELETE: api/ApiWithActions/id
+        [HttpDelete("{id}")]
+        public bool Delete(string id)
+            => repository.Delete(id);
+
+
+        // DELETE: api/ApiWithActions
+        [HttpDelete("{id}")]
+        public bool Delete(Category organization)
+            => repository.Delete(organization);
+
+
+        // DELETE: api/ApiWithActions/id
+        [HttpDelete("{id}")]
+        public bool Delete(IEnumerable<Category> organizations)
+            => repository.Delete(organizations);
 
     }
 }
