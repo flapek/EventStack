cea163b61a800d57a124a25eb7ce28865a79f37b
k0l099 <tomaszkolodziej96@gmail.com>
2020-04-18 01:55:41 +0200

    Add unitTest IsDescriptionHasMaximumOfCharacters

--- EventStack_API.UnitTest/EventTest/DescriptionTest.cs
+++ EventStack_API.UnitTest/EventTest/DescriptionTest.cs
@@ -7,8 +7,15 @@ namespace EventStack_API.UnitTest.EventTest
     public class DescriptionTest
     {
         private Event eventModel;
-        
+
         [SetUp]
         public void SetUp() => eventModel = new Event();
+
+        [Test]
+        public void Event_IsDescriptionHasMaximumOfCharacters_False()
+        {
+            eventModel.Description = new string('*', 1001);
+            Assert.IsFalse((eventModel as object).isValid("Description", "The maximum number"));
+        }
     }
 }
