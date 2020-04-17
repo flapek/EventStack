76f81601f471a89b2868de9d63cba94e3ecdd879
k0l099 <tomaszkolodziej96@gmail.com>
2020-04-18 01:11:57 +0200

    Create class DescriptionTest and SetUp method

---
+++ EventStack_API.UnitTest/EventTest/DescriptionTest.cs
@@ -0,0 +1,14 @@
+using EventStack_API.Models;
+using NUnit.Framework;
+using EventStack_API.UnitTest.Helpers;
+
+namespace EventStack_API.UnitTest.EventTest
+{
+    public class DescriptionTest
+    {
+        private Event eventModel;
+        
+        [SetUp]
+        public void SetUp() => eventModel = new Event();
+    }
+}
