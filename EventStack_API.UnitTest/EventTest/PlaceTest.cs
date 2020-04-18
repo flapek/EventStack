7248aca5167dc97e517205a72d8a9ee68eac2144
k0l099 <tomaszkolodziej96@gmail.com>
2020-04-18 06:13:02 +0200

    Add unitTest IsPlaceRequired

--- EventStack_API.UnitTest/EventTest/PlaceTest.cs
+++ EventStack_API.UnitTest/EventTest/PlaceTest.cs
@@ -10,5 +10,12 @@ namespace EventStack_API.UnitTest.EventTest
 
         [SetUp]
         public void SetUp() => eventModel = new Event();
+
+        [Test]
+        public void Event_IsPlaceRequired_False()
+        {
+            eventModel.Place = null;
+            Assert.IsFalse((eventModel as object).isValid("Place", "Place for event must be set"));
+        }
     }
 }
