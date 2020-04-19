8e8273d079c538cd69f4413ea27a3f4edc8560e2
k0l099 <tomaszkolodziej96@gmail.com>
2020-04-19 12:31:48 +0200

    Add SetUp method to EventTest class

--- EventStack_API.UnitTest/Model/EventTest.cs
+++ EventStack_API.UnitTest/Model/EventTest.cs
@@ -1,7 +1,14 @@
+using EventStack_API.Models;
+using NUnit.Framework;
+using EventStack_API.UnitTest.Helpers;
+
 namespace EventStack_API.UnitTest.Model
 {
     public class EventTest
     {
-        
+        private Event eventModel;
+
+        [SetUp]
+        public void SetUp() => eventModel = new Event();
     }
 }
