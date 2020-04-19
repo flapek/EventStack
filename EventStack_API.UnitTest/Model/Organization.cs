acd3d2720c53cce708e3ce86c74fe24002fc173a
DonOreo <44070308+DonOreo@users.noreply.github.com>
2020-04-19 12:03:48 +0200

    Change where is method for unitTest fo Organisation

--- EventStack_API.UnitTest/Model/Organization.cs
+++ EventStack_API.UnitTest/Model/Organization.cs
@@ -1,7 +1,21 @@
+using EventStack_API.Models;
+using NUnit.Framework;
+using EventStack_API.UnitTest.Helpers;
+
 namespace EventStack_API.UnitTest.Model
 {
     public class Organization
     {
-        
+        private Organization organization;
+
+        [SetUp]
+        public void SetUp() => organization = new Organization();
+
+        [Test]
+        public void Organization_IsDestriptionHasMaximumOfCharacters_False()
+        {
+            organization.Destription = new string('*', 1001);
+            Assert.IsFalse((organization as object).isValid("Destription", "The maximum number"));
+        }
     }
 }
