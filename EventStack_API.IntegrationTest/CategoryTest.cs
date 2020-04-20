b2a81dc455e43727840639e480b4e04fdb020b43
flapek <46303285+flapek@users.noreply.github.com>
2020-04-20 12:58:57 +0200

    integration test for get method

--- EventStack_API.IntegrationTest/CategoryTest.cs
+++ EventStack_API.IntegrationTest/CategoryTest.cs
@@ -6,6 +6,8 @@ using FluentAssertions;
 using Newtonsoft.Json;
 using EventStack_API.Models;
 using System.Net;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace EventStack_API.IntegrationTest
 {
@@ -20,8 +22,10 @@ namespace EventStack_API.IntegrationTest
             client = factory.CreateClient();
         }
 
+        #region Get method
+
         [Test]
-        public async Task Get_ChcekRensponseMessage_ReturnStatus200()
+        public async Task Get_All_ChcekRensponseStatusCode_ReturnStatus200()
         {
             var url = "/api/Category";
             var httpRensponse = await client.GetAsync(url);
@@ -31,8 +35,38 @@ namespace EventStack_API.IntegrationTest
             httpRensponse.StatusCode.Should().Be(HttpStatusCode.OK);
         }
 
+        [Theory]
+        public async Task Get_ById_ChcekRensponseContent_ReturnShouldNotBeNull()
+        {
+            var url = "/api/Category/";
+            var httpRensponseAll = await client.GetAsync(url);
+            var content = JsonConvert.DeserializeObject<List<Category>>(await httpRensponseAll.Content.ReadAsStringAsync());
+            var oneCategory = content.FirstOrDefault();
+            httpRensponseAll.EnsureSuccessStatusCode();
+
+            Assume.That(oneCategory != null);
+            var httpRensponse = await client.GetAsync(url + oneCategory.Id);
+
+            httpRensponse.EnsureSuccessStatusCode();
+
+            httpRensponse.Content.Should().NotBeNull();
+        }
+
+        [TestCase("d328hdn8s9auy3d")]
+        [TestCase("5e9d7e2e1c9d44000007@088")]
+        public async Task Get_ById_ChcekRensponseStatusCode_ReturnStatus400(string id)
+        {
+            var url = "/api/Category/" + id;
+            var httpRensponse = await client.GetAsync(url);
+
+            httpRensponse.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+        }
+
+
+        #endregion
+
         [Test]
-        public async Task Test1()
+        public async Task post()
         {
             var url = "/api/Category";
             HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(new Category
@@ -45,7 +79,6 @@ namespace EventStack_API.IntegrationTest
             httpRensponse.EnsureSuccessStatusCode();
 
             Assert.IsTrue(httpRensponse.IsSuccessStatusCode);
-
         }
     }
 }
