ce5da523a287dcd535ed5376c36cf3af84f44aad
flapek <46303285+flapek@users.noreply.github.com>
2020-04-21 09:44:44 +0200

    comment unnecesery method for this moment

--- EventStack_API.IntegrationTest/EventTest.cs
+++ EventStack_API.IntegrationTest/EventTest.cs
@@ -66,156 +66,156 @@ namespace EventStack_API.IntegrationTest
 
         #endregion
 
-        #region Post method
+        //#region Post method
 
-        [Test]
-        public async Task Post_CheckRensponseStatusCodeWhenModelIsValid_ReturnStatus200()
-        {
-            var url = "/api/Event";
-            HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(new Category
-            {
-                Name = "Holidays"
-            }), Encoding.UTF8, "application/json");
+        //[Test]
+        //public async Task Post_CheckRensponseStatusCodeWhenModelIsValid_ReturnStatus200()
+        //{
+        //    var url = "/api/Event";
+        //    HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(new Category
+        //    {
+        //        Name = "Holidays"
+        //    }), Encoding.UTF8, "application/json");
 
-            var httpRensponse = await client.PostAsync(url, httpContent);
+        //    var httpRensponse = await client.PostAsync(url, httpContent);
 
-            httpRensponse.EnsureSuccessStatusCode();
+        //    httpRensponse.EnsureSuccessStatusCode();
 
-            httpRensponse.StatusCode.Should().Be(HttpStatusCode.OK);
-        }
+        //    httpRensponse.StatusCode.Should().Be(HttpStatusCode.OK);
+        //}
 
-        [Test]
-        public async Task Post_CheckRensponseStatusCodeWhenNameIsNotSet_ReturnStatus400()
-        {
-            var url = "/api/Event";
-            HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(new Category()), Encoding.UTF8, "application/json");
+        //[Test]
+        //public async Task Post_CheckRensponseStatusCodeWhenNameIsNotSet_ReturnStatus400()
+        //{
+        //    var url = "/api/Event";
+        //    HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(new Category()), Encoding.UTF8, "application/json");
 
-            var httpRensponse = await client.PostAsync(url, httpContent);
+        //    var httpRensponse = await client.PostAsync(url, httpContent);
 
-            httpRensponse.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-        }
+        //    httpRensponse.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        //}
 
-        [TestCase("sadnvfinoisdqwdnwoqkncionocesjoisadoisamkdnowqidnewonckoicoiocnewoinvksmocpjeionfcodsmopmowen")]
-        public async Task Post_CheckRensponseStatusCodeWhenNameIsLongerThan50_ReturnStatus400(string name)
-        {
-            var url = "/api/Event";
-            HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(new Category
-            {
-                Name = name
-            }), Encoding.UTF8, "application/json");
+        //[TestCase("sadnvfinoisdqwdnwoqkncionocesjoisadoisamkdnowqidnewonckoicoiocnewoinvksmocpjeionfcodsmopmowen")]
+        //public async Task Post_CheckRensponseStatusCodeWhenNameIsLongerThan50_ReturnStatus400(string name)
+        //{
+        //    var url = "/api/Event";
+        //    HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(new Category
+        //    {
+        //        Name = name
+        //    }), Encoding.UTF8, "application/json");
 
-            var httpRensponse = await client.PostAsync(url, httpContent);
+        //    var httpRensponse = await client.PostAsync(url, httpContent);
 
-            httpRensponse.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-        }
+        //    httpRensponse.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        //}
 
-        #endregion
+        //#endregion
 
-        #region Put method
+        //#region Put method
 
-        [Theory]
-        public async Task Put_CheckRensponseStatusCodeWhenModelIsValid_ReturnStatus200()
-        {
-            var url = "/api/Event/";
-            var httpRensponseAll = await client.GetAsync(url);
-            var content = JsonConvert.DeserializeObject<List<Category>>(await httpRensponseAll.Content.ReadAsStringAsync());
-            var oneCategory = content.FirstOrDefault();
-            httpRensponseAll.EnsureSuccessStatusCode();
+        //[Theory]
+        //public async Task Put_CheckRensponseStatusCodeWhenModelIsValid_ReturnStatus200()
+        //{
+        //    var url = "/api/Event/";
+        //    var httpRensponseAll = await client.GetAsync(url);
+        //    var content = JsonConvert.DeserializeObject<List<Category>>(await httpRensponseAll.Content.ReadAsStringAsync());
+        //    var oneCategory = content.FirstOrDefault();
+        //    httpRensponseAll.EnsureSuccessStatusCode();
 
-            Assume.That(oneCategory != null);
-            url += oneCategory.Id;
-            HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(new Category
-            {
-                Name = "changedName"
-            }), Encoding.UTF8, "application/json");
-            var httpRensponse = await client.PutAsync(url, httpContent);
-            httpRensponse.EnsureSuccessStatusCode();
+        //    Assume.That(oneCategory != null);
+        //    url += oneCategory.Id;
+        //    HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(new Category
+        //    {
+        //        Name = "changedName"
+        //    }), Encoding.UTF8, "application/json");
+        //    var httpRensponse = await client.PutAsync(url, httpContent);
+        //    httpRensponse.EnsureSuccessStatusCode();
 
-            httpRensponse.StatusCode.Should().Be(HttpStatusCode.OK);
-        }
+        //    httpRensponse.StatusCode.Should().Be(HttpStatusCode.OK);
+        //}
 
-        [Theory]
-        public async Task Put_CheckRensponseContentWhenModelIsValid_ReturnNameIsCorrectChanged()
-        {
-            var expected = new Category { Name = "changedName" };
-            var url = "/api/Event/";
-            var httpRensponseAll = await client.GetAsync(url);
-            var content = JsonConvert.DeserializeObject<List<Category>>(await httpRensponseAll.Content.ReadAsStringAsync());
-            var oneCategory = content.FirstOrDefault();
-            httpRensponseAll.EnsureSuccessStatusCode();
+        //[Theory]
+        //public async Task Put_CheckRensponseContentWhenModelIsValid_ReturnNameIsCorrectChanged()
+        //{
+        //    var expected = new Category { Name = "changedName" };
+        //    var url = "/api/Event/";
+        //    var httpRensponseAll = await client.GetAsync(url);
+        //    var content = JsonConvert.DeserializeObject<List<Category>>(await httpRensponseAll.Content.ReadAsStringAsync());
+        //    var oneCategory = content.FirstOrDefault();
+        //    httpRensponseAll.EnsureSuccessStatusCode();
 
-            Assume.That(oneCategory != null);
-            url += oneCategory.Id;
-            HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(expected), Encoding.UTF8, "application/json");
-            var httpRensponse = await client.PutAsync(url, httpContent);
-            httpRensponse.EnsureSuccessStatusCode();
+        //    Assume.That(oneCategory != null);
+        //    url += oneCategory.Id;
+        //    HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(expected), Encoding.UTF8, "application/json");
+        //    var httpRensponse = await client.PutAsync(url, httpContent);
+        //    httpRensponse.EnsureSuccessStatusCode();
 
-            var result = JsonConvert.DeserializeObject<Category>(await httpRensponse.Content.ReadAsStringAsync());
-            Assert.AreEqual(expected.Name, result.Name);
-        }
+        //    var result = JsonConvert.DeserializeObject<Category>(await httpRensponse.Content.ReadAsStringAsync());
+        //    Assert.AreEqual(expected.Name, result.Name);
+        //}
 
-        [TestCase("5e9d7e2e1c9d44000007a088s")]
-        [TestCase("5e9d7e2e1c9d44000007a")]
-        [TestCase("5e9d7e2e1c9d44000007@088")]
-        public async Task Put_CheckRensponseStatusCodeWhenIdIsNotValid_ReturnStatus500(string id)
-        {
-            var url = "/api/Event/" + id;
-            HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(new Category
-            {
-                Name = "changedName"
-            }), Encoding.UTF8, "application/json");
-            var httpRensponse = await client.PutAsync(url, httpContent);
+        //[TestCase("5e9d7e2e1c9d44000007a088s")]
+        //[TestCase("5e9d7e2e1c9d44000007a")]
+        //[TestCase("5e9d7e2e1c9d44000007@088")]
+        //public async Task Put_CheckRensponseStatusCodeWhenIdIsNotValid_ReturnStatus500(string id)
+        //{
+        //    var url = "/api/Event/" + id;
+        //    HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(new Category
+        //    {
+        //        Name = "changedName"
+        //    }), Encoding.UTF8, "application/json");
+        //    var httpRensponse = await client.PutAsync(url, httpContent);
 
-            httpRensponse.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
-        }
+        //    httpRensponse.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+        //}
 
-        #endregion
+        //#endregion
 
-        #region Delete method
+        //#region Delete method
 
-        [Theory]
-        public async Task Delete_CheckRensponseStatusCodeWhenIdIsCorrect_ReturnStatus200()
-        {
-            var url = "/api/Event/";
-            var httpRensponseAll = await client.GetAsync(url);
-            var content = JsonConvert.DeserializeObject<List<Category>>(await httpRensponseAll.Content.ReadAsStringAsync());
-            var oneCategory = content.FirstOrDefault();
-            httpRensponseAll.EnsureSuccessStatusCode();
+        //[Theory]
+        //public async Task Delete_CheckRensponseStatusCodeWhenIdIsCorrect_ReturnStatus200()
+        //{
+        //    var url = "/api/Event/";
+        //    var httpRensponseAll = await client.GetAsync(url);
+        //    var content = JsonConvert.DeserializeObject<List<Category>>(await httpRensponseAll.Content.ReadAsStringAsync());
+        //    var oneCategory = content.FirstOrDefault();
+        //    httpRensponseAll.EnsureSuccessStatusCode();
 
-            Assume.That(oneCategory != null);
-            url += oneCategory.Id;
-            var httpRensponse = await client.DeleteAsync(url);
+        //    Assume.That(oneCategory != null);
+        //    url += oneCategory.Id;
+        //    var httpRensponse = await client.DeleteAsync(url);
 
-            httpRensponse.StatusCode.Should().Be(HttpStatusCode.OK);
-        }
+        //    httpRensponse.StatusCode.Should().Be(HttpStatusCode.OK);
+        //}
 
-        [Theory]
-        public async Task Delete_CheckRensponseStatusCodeWhenIdIsCorrect_ReturnTrue()
-        {
-            var url = "/api/Event/";
-            var httpRensponseAll = await client.GetAsync(url);
-            var contentFromGet = JsonConvert.DeserializeObject<List<Category>>(await httpRensponseAll.Content.ReadAsStringAsync());
-            var oneCategory = contentFromGet.FirstOrDefault();
-            httpRensponseAll.EnsureSuccessStatusCode();
+        //[Theory]
+        //public async Task Delete_CheckRensponseStatusCodeWhenIdIsCorrect_ReturnTrue()
+        //{
+        //    var url = "/api/Event/";
+        //    var httpRensponseAll = await client.GetAsync(url);
+        //    var contentFromGet = JsonConvert.DeserializeObject<List<Category>>(await httpRensponseAll.Content.ReadAsStringAsync());
+        //    var oneCategory = contentFromGet.FirstOrDefault();
+        //    httpRensponseAll.EnsureSuccessStatusCode();
 
-            Assume.That(oneCategory != null);
-            url += oneCategory.Id;
-            var httpRensponse = await client.DeleteAsync(url);
-            var contentFromDelete = JsonConvert.DeserializeObject<bool>(await httpRensponse.Content.ReadAsStringAsync());
-            contentFromDelete.Should().BeTrue();
-        }
+        //    Assume.That(oneCategory != null);
+        //    url += oneCategory.Id;
+        //    var httpRensponse = await client.DeleteAsync(url);
+        //    var contentFromDelete = JsonConvert.DeserializeObject<bool>(await httpRensponse.Content.ReadAsStringAsync());
+        //    contentFromDelete.Should().BeTrue();
+        //}
 
-        [TestCase("5e9d7e2e1c9d44000007a088s")]
-        [TestCase("5e9d7e2e1c9d44000007a")]
-        [TestCase("5e9d7e2e1c9d44000007@088")]
-        public async Task Delete_ChcekRensponseStatusCode_ReturnFalse(string id)
-        {
-            var url = "/api/Event/" + id;
-            var httpRensponse = await client.DeleteAsync(url);
-            var content = JsonConvert.DeserializeObject<bool>(await httpRensponse.Content.ReadAsStringAsync());
-            content.Should().BeFalse();
-        }
+        //[TestCase("5e9d7e2e1c9d44000007a088s")]
+        //[TestCase("5e9d7e2e1c9d44000007a")]
+        //[TestCase("5e9d7e2e1c9d44000007@088")]
+        //public async Task Delete_ChcekRensponseStatusCode_ReturnFalse(string id)
+        //{
+        //    var url = "/api/Event/" + id;
+        //    var httpRensponse = await client.DeleteAsync(url);
+        //    var content = JsonConvert.DeserializeObject<bool>(await httpRensponse.Content.ReadAsStringAsync());
+        //    content.Should().BeFalse();
+        //}
 
-        #endregion
+        //#endregion
     }
 }
