7e2708e515b9a91bdbc5b61f5b42e30788086eec
flapek <46303285+flapek@users.noreply.github.com>
2020-04-25 21:45:41 +0200

    resolve merge conflict

--- EventStack_API/Models/Event.cs
+++ EventStack_API/Models/Event.cs
@@ -12,11 +12,11 @@ namespace EventStack_API.Models
         [BsonId]
         [BsonElement("Id")]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; }
+        public string Id { get; set; }
 
         [BsonElement("Name")]
         [BsonRequired]
-        [Required(ErrorMessage ="Name must be defined!")]
+        [Required(ErrorMessage = "Name must be defined!")]
         [StringLength(50, ErrorMessage = "The maximum number of character is 50!")]
         public string Name { get; set; }
 
@@ -35,35 +35,38 @@ namespace EventStack_API.Models
         public string Description { get; set; }
 
         [BsonElement("StartTime")]
+        [BsonRepresentation(BsonType.DateTime)]
         [BsonRequired]
         [Required(ErrorMessage = "Event start date must be defined!")]
         public DateTime StartTime { get; set; }
 
         [BsonElement("EndTime")]
+        [BsonRepresentation(BsonType.DateTime)]
         [BsonRequired]
         [Required(ErrorMessage = "Event end date must be defined!")]
         public DateTime EndTime { get; set; }
 
         [BsonElement("PublishTime")]
+        [BsonRepresentation(BsonType.DateTime)]
         [BsonRequired]
         [Required(ErrorMessage = "Publish date must be set")]
         public DateTime PublishTime { get; set; }
 
         [BsonElement("Place")]
         [BsonRequired]
-        [Required(ErrorMessage ="Place for event must be set")]
+        [Required(ErrorMessage = "Place for event must be set")]
         public Address Place { get; set; }
-        
+
         [BsonElement("IsCanceled")]
         [BsonDefaultValue(false)]
         [BsonRequired]
         [Required(ErrorMessage = "Flag must be set!")]
         public bool IsCanceled { get; set; }
-        
+
         [BsonElement("FacebookURL")]
-        public Uri FacebookURL { get; set; }
-        
+        public string FacebookURL { get; set; }
+
         [BsonElement("WebSiteURL")]
-        public Uri WebSiteURL { get; set; }
+        public string WebSiteURL { get; set; }
     }
 }
