86ed7f26a05a70fec252bc805415bb57a26c1ed2
Sherlock <tomekwator1997@gmail.com>
2020-05-04 01:12:47 +0200

    Add test is name required

--- EventStack_API.UnitTest/Model/CategoryTest.cs
+++ EventStack_API.UnitTest/Model/CategoryTest.cs
@@ -11,6 +11,13 @@ namespace EventStack_API.UnitTest.Model
         public void SetUp() => categoryModel = new Category();
 
         [Test]
+        public void Category_IsNameRequired_False()
+        {
+            categoryModel.Name = null;
+            Assert.IsFalse((categoryModel as object).isValid("Name", "Name must be set!"));
+        }
+
+        [Test]
         public void Category_IsNameHasMaximumOfCharacters_False()
         {
             categoryModel.Name = new string('*', 51);
