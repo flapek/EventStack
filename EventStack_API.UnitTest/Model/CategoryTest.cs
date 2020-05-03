5cc2c70ef9f5b62ba4f64aaa27fe70d5d864ebdb
Sherlock <tomekwator1997@gmail.com>
2020-05-03 19:15:46 +0200

    Add setUp method

--- EventStack_API.UnitTest/Model/CategoryTest.cs
+++ EventStack_API.UnitTest/Model/CategoryTest.cs
@@ -1,7 +1,12 @@
+using EventStack_API.Models;
+using NUnit.Framework;
+using EventStack_API.UnitTest.Helpers;
+
 namespace EventStack_API.UnitTest.Model
 {
     public class CategoryTest
     {
-        
+        [SetUp]
+        public void SetUp() => categoryModel = new Category();
     }
 }
