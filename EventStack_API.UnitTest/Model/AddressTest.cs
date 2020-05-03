ae34825691bb160cf76d646ba50151dfbc12be4c
Sherlock <tomekwator1997@gmail.com>
2020-05-03 14:03:03 +0200

    Create class AddressTest

---
+++ EventStack_API.UnitTest/Model/AddressTest.cs
@@ -0,0 +1,10 @@
+using EventStack_API.Models;
+using NUnit.Framework;
+using EventStack_API.UnitTest.Helpers;
+
+namespace EventStack_API.UnitTest.Model
+{
+    public class AddressTest
+    {
+    }
+}
