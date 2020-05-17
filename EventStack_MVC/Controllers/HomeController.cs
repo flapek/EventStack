b0c33299010e07b9a66fcc5af2d8670d5be7e77d
Sherlock <tomekwator1997@gmail.com>
2020-05-17 09:33:30 +0200

    Add Index method homeController for mvc

--- EventStack_MVC/Controllers/HomeController.cs
+++ EventStack_MVC/Controllers/HomeController.cs
@@ -1,22 +1,44 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using EventStack_MVC.Models;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
 
 namespace EventStack_MVC.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private HttpClient client;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            client = new HttpClient();
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Index(string exampleDataFilter)
+        {
+            var eventList = new List<Event>();
+
+            var url = "api/Event/GetAll";
+            var httpRensponse = await client.GetAsync(url);
+
+            if (httpRensponse.IsSuccessStatusCode)
+            {
+                var message = httpRensponse.Content.ReadAsStreamAsync().Result;
+                eventList = await JsonSerializer.DeserializeAsync<List<Event>>(message);
+            }
+
+            return View(eventList);
         }
 
         public IActionResult Privacy()
