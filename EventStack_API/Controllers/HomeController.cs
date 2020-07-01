461872feaf74a456993164ae57c47ca077b3ebee
Sherlock <tomekwator1997@gmail.com>
2020-07-01 03:53:34 +0200

    Add test adding Event to database

--- EventStack_API/Controllers/HomeController.cs
+++ EventStack_API/Controllers/HomeController.cs
@@ -9,16 +9,22 @@ using Microsoft.AspNetCore.Identity;
 using EventStack_API.Models;
 using EventStack_API.Workers;
 using EventStack_API.Interfaces;
+using System;
+using System.Net;
 
 namespace EventStack_API.Controllers
 {
     public class HomeController : Controller
     {
-        private Organization_MongoRepository repository { get; set; }
+        private Organization_MongoRepository repositoryOrganization { get; set; }
+        private Event_MongoRepository repositoryEvent { get; set; }
+        private Category_MongoRepository repositoryCategory { get; set; }
 
-        public HomeController(IRepositoryFactory<Organization> repository)
+
+        public HomeController(IRepositoryFactory<Organization> repositoryOrganization, IRepositoryFactory<Event> repositoryEvent)
         {
-            this.repository = (Organization_MongoRepository)repository;
+            this.repositoryOrganization = (Organization_MongoRepository)repositoryOrganization;
+            this.repositoryEvent = (Event_MongoRepository)repositoryEvent;
         }
 
         public IActionResult Index()
@@ -44,9 +50,38 @@ namespace EventStack_API.Controllers
                 PhoneNumber = "666666666"
             };
 
-            repository.Insert(goodOrganization);
+            repositoryOrganization.Insert(goodOrganization);
             
             return View();
         }
+
+        public IActionResult AddEvent()
+        {
+            WebClient webClient = new WebClient();
+            Event goodEvent = new Event
+            {
+                Name = "Test",
+                Photo = webClient.DownloadData("http://www.google.com/images/logos/ps_logo2.png"),
+                StartTime = DateTime.Now.AddDays(20),
+                EndTime = DateTime.Now.AddDays(22),
+                PublishTime = DateTime.Now,
+                Place = new Address
+                {
+                    City = "Warsaw",
+                    Country = "Poland",
+                    Street = "Test 11/4",
+                    ZipCode = "43-333",
+                },
+                IsCanceled = false,
+                Description = "some description",
+                FacebookURL = "http://www.google.com",
+                WebSiteURL = "http://www.google.com"
+            };
+
+            repositoryEvent.Insert(goodEvent);
+            
+            return View();
+        }
+
     }
 }
